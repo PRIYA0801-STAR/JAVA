16fe17c54f520674e6aee15c654f21afd44d22df
PRIYA0801-STAR <sambathsumi2004@gmail.com>
2025-07-30 14:09:40 +0530

    Add files via upload

---
+++ adding two variables.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int a=10;
+	    int b=8;
+	    int c=a+b;
+	    System.out.println(c);
+	}
+}
