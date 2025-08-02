c97e767e7694dea5c4d68963b52a1b58108e9b5c
PRIYA0801-STAR <sambathsumi2004@gmail.com>
2025-08-02 11:25:25 +0530

    Add files via upload

---
+++ basic login using username and password.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args){
+	    int username = 345;
+	    int password = 14;
+	    if (username == 123 && password == 14){
+	        System.out.println(" login successfull");
+	    }
+	    else{
+	        System.out.println( " login failed");
+	    }
+	    }
+}
+	    
+	
