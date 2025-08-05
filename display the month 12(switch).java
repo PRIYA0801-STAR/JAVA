14de68b18ceaaa496939c6654fb3e30e380397d4
PRIYA0801-STAR <sambathsumi2004@gmail.com>
2025-08-05 19:12:48 +0530

    program using switch to display the month based number

---
+++ display the month 12(switch).java
@@ -0,0 +1,66 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		int months = 1;
+		switch (months){
+		    case 1:
+		        System.out.println("January");
+		        break;
+		    case 2:
+		        System.out.println("February");
+		        break;
+		    case 3:
+		        System.out.println("March");
+		        break;
+		    case 4:
+		       System.out.println("April");
+		       break;
+		    case 5:
+		       System.out.println("May");
+		       break;
+		    case 6:
+		        System.out.println("June");
+		        break;
+		    case 7:
+		       System.out.println("July");
+		       break;
+		    case 8:
+		       System.out.println("August");
+		       break;
+		    case 9:
+		       System.out.println("Setember");
+		       break;
+		    case 10:
+		      System.out.println("October");
+		      break;
+		    case 11:
+		      System.out.println("November");
+		      break;
+	   	 case 12:
+		     System.out.println("December");
+		     break;
+		  default:
+		  System.out.println("invalid month");
+		}
+	}
+}
+		  
+		   
+		    
+		   
+		    
+		    
+		    
+		
+		
+		
+		
+	
