6693123d11df7d1493143d7447f1624f572a5a7e
PRIYA0801-STAR <sambathsumi2004@gmail.com>
2025-08-29 20:52:44 +0530

    Adding the array numbers

---
+++ Adding array.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int sum = 0;
+		int [] num = {2,8,8,7};
+		for(int i=0; i<num.length; i++){
+		    sum = sum + num[i];
+		}
+		System.out.println(sum);
+	}
+}
